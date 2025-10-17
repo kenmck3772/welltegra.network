860374b1b07a2c2ab4d20487f336cd353755ee23
kenmck3772 <48637368+kenmck3772@users.noreply.github.com>
2025-10-18 00:05:50 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,8 @@
+module.exports = {
+  content: ["./**/*.html", "./**/*.{js,ts}"],
+  theme: { extend: {} },
+  plugins: [],
+}
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./**/*.html", "./src/**/*.{js,ts}"],
