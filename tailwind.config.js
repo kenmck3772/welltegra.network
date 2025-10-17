41b34f42ff51cb472734331295b87acc15623ad2
kenmck3772 <48637368+kenmck3772@users.noreply.github.com>
2025-10-17 22:46:57 +0100

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.html", "./**/*.js"],
+  content: ["./**/*.html", "./src/**/*.{js,ts}"],
   theme: { extend: {} },
-  plugins: []
+  plugins: [],
 };
